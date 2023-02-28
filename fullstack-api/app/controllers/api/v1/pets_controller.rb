aaa1ad80e125d6654a384c66d1b06d0295a8c8ca
Ben Connolly <benconnollyofficial@gmail.com>
2023-02-28 16:19:03 +0000

    Update pets controller and add first spec

--- fullstack-api/app/controllers/api/v1/pets_controller.rb
+++ fullstack-api/app/controllers/api/v1/pets_controller.rb
@@ -3,12 +3,10 @@
 module Api
   module V1
     class PetsController < ApplicationController
-
       def index
-        pets = []
+        pets = Pet.all.order(:featured, :name)
         render json: pets, status: :ok
       end
-
     end
   end
 end

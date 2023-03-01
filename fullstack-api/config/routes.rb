f0113ae33290355087e93f70c7f0fc7314aeda89
Ben Connolly <benconnollyofficial@gmail.com>
2023-03-01 14:44:50 +0000

    Add devise and setup user model

--- fullstack-api/config/routes.rb
+++ fullstack-api/config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   namespace :api do
     namespace :v1 do
       get 'pets', to: 'pets#index', as: 'pets'
     end
   end
-
 end
